f749005db4f2cd2904e36c00d9a8faf64e927b06
luy <seven_ou@163.com>
2025-10-05 00:06:09 +0800

    1

--- reaper_strategy.py
+++ reaper_strategy.py
@@ -617,7 +617,7 @@ class DynamicVolatilityGridReaper:
         try:
             response = await self.lighter.candlesticks(
                 self.symbol,
-                resolution="1h",
+                resolution="5m",
                 count_back=self.candle_lookback,
                 set_timestamp_to_end=False,
             )
